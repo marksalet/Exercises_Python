1fc99018342753d245ddd5d3fe9bf3f0ce62343e
Mark <500306@edu.rocmn.nl>
2018-09-13 14:49:43 +0200

    Add Python Exercises files

--- PROG4_3.py
+++ PROG4_3.py
@@ -3,4 +3,4 @@ urenGewerkt = float(input("Vul in hoeveel uur je gewerkt hebt: "))
 
 kosten = uurloon * urenGewerkt
 
-print("Je salaris: €" ,round(kosten ,2))
+print("Je salaris: €" ,round(kosten ,2))vesev
